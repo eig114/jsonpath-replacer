45922c85eb8b43e738f7a03ec5f50f5cbd24f005
eig114 <eig114@gmail.com>
2022-06-20 18:28:48 +0300

    mess around with project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
+(def slf4j-version "1.7.36")
+
 (defproject jsonpath-replacer "0.1.0-SNAPSHOT"
   :description "Replace values defined by JSONPath"
   :url "https://github.com/eig114/jsonpath-replacer"
@@ -6,15 +8,17 @@
                  [org.clojure/tools.cli "1.0.206"]
                  [com.jayway.jsonpath/json-path "2.4.0"]
                  ;;slf4j needed for jsonpath
-                 [org.slf4j/slf4j-api "1.7.36"]
+                 [org.slf4j/slf4j-api ~slf4j-version]
                  ]
   :profiles {
              ;; Only include NOP logger when building executable
              ;; uberjar or doing dev stuff. Otherwise assume user will
-             ;; have their own slf4j implementation on classpath.  see
-             ;; https://www.slf4j.org/codes.html#StaticLoggerBinder
-             :uberjar {:dependencies [[org.slf4j/slf4j-nop "1.7.36"]]}
-             :dev     {:dependencies [[org.slf4j/slf4j-nop "1.7.36"]]}
+             ;; have their own slf4j implementation on classpath.
+             ;; see https://www.slf4j.org/codes.html#StaticLoggerBinder
+             :uberjar {:dependencies [[org.slf4j/slf4j-nop ~slf4j-version]]
+                       :jvm-opts ["-Dclojure.compiler.elide-meta=[:doc :file :line :added]"
+                                  "-Dclojure.compiler.direct-linking=true"]}
+             :dev     {:dependencies [[org.slf4j/slf4j-nop ~slf4j-version]]}
              }
   :plugins [[com.github.clj-kondo/lein-clj-kondo "0.2.0"]]
   :repl-options {:init-ns jsonpath-replacer.core}
